a1182ac79ec6c243d41844ea1274cc49daa4a863
Vivi Poit <vwpoit@gmail.com>
2024-03-31 22:57:44 -0400

    add more unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const tagline = screen.getByText('This app will soon be another React tic-tac-toe game.');
-  expect(tagline).toBeInTheDocument();
-});
+test('renders a header', () => {
+  render(<App />)
+  const header = document.querySelector('header')
+  expect(header).toBeInTheDocument()
+})
+
+test('renders app (React) logo', () => {
+  render(<App />)
+  const appLogo = document.querySelector('img.App-logo')
+  expect(appLogo).toBeInTheDocument()
+})
+
+test('renders app (React) logo with value in alt attribute', () => {
+  render(<App />)
+  const appLogo = document.querySelector('img.App-logo')
+  expect(appLogo).toHaveAttribute('alt', 'logo')
+})
+
+test('renders custom tagline', () => {
+  render(<App />)
+  const tagline = screen.getByText('This app will soon be another React tic-tac-toe game.')
+  expect(tagline).toBeInTheDocument()
+})
+
+test('does not render any links', () => {
+  render(<App />)
+  const learnReactLink = document.querySelector('a')
+  expect(learnReactLink).not.toBeInTheDocument()
+})
